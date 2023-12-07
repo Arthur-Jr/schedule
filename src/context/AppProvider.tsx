a46c403b057fc2e5df67d1972151ec0142cf77fb
Arthur Junior <arthurjr1906@gmail.com>
2023-12-07 18:05:36 -0300

    feat: context implement on searchBar component

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Show from '@/interfaces/Show';
-import { Dispatch, SetStateAction, createContext, useState } from 'react';
+import { Dispatch, SetStateAction, createContext, useEffect, useState } from 'react';
 
 interface Context {
   showsOnSchedule: [] | Show[],
@@ -12,6 +12,13 @@ export const appContext = createContext<Context>({} as Context);
 
 function AppProvider({ children }: { children: React.ReactNode }) {
   const [showsOnSchedule, setShowsOnSchedule] = useState<Show[] | []>([]);
+
+  useEffect(() => {
+    const shows = localStorage.getItem('showSchedule');
+    if (shows) {
+      setShowsOnSchedule(JSON.parse(shows));
+    }
+  }, []);
 
   return (
     <appContext.Provider value={{ showsOnSchedule, setShowsOnSchedule }}>
