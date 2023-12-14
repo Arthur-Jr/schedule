52647eee3f5fd9f07108d6f1541c467c15ff3413
Arthur Junior <arthurjr1906@gmail.com>
2023-12-14 16:22:13 -0300

    feat: Create Schedule main content component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import Header from './Header';
+import ScheduleMainContent from './ScheduleMainContent';
 import SearchBar from './SearchBar';
 
-export { Header, SearchBar };
+export { Header, ScheduleMainContent, SearchBar };
+
