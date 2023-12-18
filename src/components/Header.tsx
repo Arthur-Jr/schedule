b09e009fb9de9baba6a1e4b67f81e52fbffad348
Arthur Junior <arthurjr1906@gmail.com>
2023-12-18 20:30:55 -0300

    feat: Create user navBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,14 @@
-'use client';
-
 import { Kaushan_Script } from 'next/font/google';
-import { useState } from 'react';
-import LoginForm from './LoginForm';
+import UserNavBar from './UserNavBar';
 
 const kaushan = Kaushan_Script({ weight: '400', subsets: ['latin'] });
 
 export default function Header() {
-  const [isLogging, setIsLogging] = useState(false);
-
-  const handleRegister = () => {
-    const page = process.env.REGISTER_PAGE_URL || '';
-    window.open(page , 'register', 'toolbar=0,status=0,width=800,height=800');
-  };
-
   return (
     <header className="flex items-center w-full h-20 py-5 justify-center relative border-b-zinc-800 border-b-[1px]">
       <h1 className={`${kaushan.className} text-4xl italic cursor-default self`}>Schedule</h1>
 
-      <div className="absolute right-1 w-20 h-full flex flex-col justify-center items-start gap-2">
-        <button
-          type="button"
-          onClick={ () => setIsLogging(!isLogging) }
-          className="italic hover:underline underline-offset-2"
-        >
-          { isLogging ? 'Cancel' : 'Login' }
-        </button>
-
-        <button
-          type="button"
-          onClick={ () => handleRegister() }
-          className="italic hover:underline underline-offset-2"
-        >
-          Register
-        </button>
-      </div>
-
-      { isLogging && <LoginForm setIsLogging={ setIsLogging } /> }
+      <UserNavBar />
     </header>
   );
 };
---
+++ src/components/UserNavBar.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import constants from '@/constants/constants';
+import { appContext } from '@/context/AppProvider';
+import { useContext, useState } from 'react';
+import LoginForm from './LoginForm';
+
+export default function UserNavBar() {
+  const [isLogging, setIsLogging] = useState(false);
+  const { isLogged, setIsLogged, setShowsOnSchedule } = useContext(appContext);
+
+  const handleRegister = () => {
+    const page = process.env.REGISTER_PAGE_URL || '';
+    window.open(page , 'register', 'toolbar=0,status=0,width=800,height=800');
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem(constants.userTokenStorageKey);
+    setShowsOnSchedule([]);
+    setIsLogged(false);
+  }
+
+  return (
+    <>
+      <nav className="absolute right-1 w-20 h-full">
+        <div className="h-full flex flex-col justify-center items-start gap-2">
+          { !isLogged && 
+            <button
+              type="button"
+              onClick={ () => setIsLogging(!isLogging) }
+              className="italic hover:underline underline-offset-2"
+            >
+              { isLogging ? 'Cancel' : 'Login' }
+            </button>
+          }
+
+          { isLogged &&
+            <button
+              type="button"
+              onClick={ () => handleLogout() }
+              className="italic hover:underline underline-offset-2"
+            >
+              Logout
+            </button>
+          }
+
+          <button
+            type="button"
+            onClick={ () => handleRegister() }
+            className="italic hover:underline underline-offset-2"
+          >
+            { isLogged ? 'Account' : 'Register' }
+          </button>
+        </div>
+      </nav>
+  
+      { isLogging && <LoginForm setIsLogging={ setIsLogging } /> }
+    </>
+  );
+}
