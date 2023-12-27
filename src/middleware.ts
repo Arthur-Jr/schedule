6ecc99800385f56f484dd534107c0c84b7d6b0ce
Arthur Junior <arthurjr1906@gmail.com>
2023-12-26 23:06:54 -0300

    feat: create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { HttpStatusCode } from 'axios';
+import * as jwt from 'jose';
+import { NextRequest, NextResponse } from 'next/server';
+import constants from './constants/constants';
+
+interface jwtCustomPayload extends jwt.JWTVerifyResult<jwt.JWTPayload> {
+  payload: {
+    data: { username: string },
+  }
+}
+
+export async function middleware(req: NextRequest): Promise<NextResponse | undefined> {
+  try {
+    const token = req.cookies.get(constants.userTokenStorageKey)?.value;
+    const secret = new TextEncoder().encode(process.env.JWT_KEY || '');
+    
+    if (!token) {
+      const response = NextResponse.next();
+      response.cookies.delete(constants.userTokenStorageKey);
+      return NextResponse.json({ status: HttpStatusCode.Unauthorized, message: 'Invalid Token!' });
+    }
+
+    const data = await jwt.jwtVerify(token, secret) as jwtCustomPayload;
+    const newHeaders = new Headers(req.headers);
+    newHeaders.set('username', data.payload.data.username);
+
+    return NextResponse.next({
+      request: {
+        headers: newHeaders,
+      }
+    });
+  } catch(err) {
+    const response = NextResponse.next();
+    response.cookies.delete(constants.userTokenStorageKey);
+    return NextResponse.json({ status: HttpStatusCode.Unauthorized, message: 'Invalid Token!' });
+  }
+}
