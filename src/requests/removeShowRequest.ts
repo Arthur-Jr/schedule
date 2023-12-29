88defff28b55b06774448bd9a379f0fdca771158
Arthur Junior <arthurjr1906@gmail.com>
2023-12-29 12:23:28 -0300

    fix: change request to send token through header

--- src/requests/removeShowRequest.ts
+++ src/requests/removeShowRequest.ts
@@ -2,13 +2,13 @@ import Show from '@/interfaces/Show';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
 
-export default async function removeShowRequest(show: Show): Promise<AxiosResponse> {
+export default async function removeShowRequest(show: Show, token: string): Promise<AxiosResponse> {
   try {
     const response = await axios.put(`/api/show`, show, {
-      withCredentials: true,
       timeout: 10000,
       headers: { 
         'content-type': 'application/json',
+        'Authorization': token,
       },
     });
 
